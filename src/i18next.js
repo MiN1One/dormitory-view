05c56e3cac05428171938fac80c7cb6ea3bc51d9
MiN1One <git_min1one@mail.ru>
2021-04-13 12:18:30 +0500

    Initialized project

---
+++ src/i18next.js
@@ -0,0 +1,18 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import languageDetector from 'i18next-browser-languagedetector';
+import backend from 'i18next-http-backend';
+
+i18n
+  .use(languageDetector)
+  .use(backend)
+  .use(initReactI18next)
+  .init({
+    load: 'currentOnly',
+    fallbackLng: false,
+    whitelist: ['en', 'ru', 'uz'],
+    react: { useSuspense: false },
+    debug: true
+  });
+
+export default i18n;
